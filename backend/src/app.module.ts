3dbb03a2b1070b2ce40208453f807ad61be32505
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-02 18:26:57 +0600

    feat(backend): enable goods and market data modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoodModule } from './goods/good.module';
+import { MarketDataModule } from './market-data.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -27,6 +29,8 @@ import { UserModule } from './user/user.module';
       }),
     }),
     UserModule,
+    GoodModule,
+    MarketDataModule,
   ],
   controllers: [AppController],
   providers: [AppService],
