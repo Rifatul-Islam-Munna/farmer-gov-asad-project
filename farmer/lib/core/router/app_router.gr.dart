890c0cedf371e2482a1743f9536ca4be109704fa
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-02 17:56:17 +0600

    chore(flutter): generate AutoRoute definitions on development

---
+++ farmer/lib/core/router/app_router.gr.dart
@@ -0,0 +1,82 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_router.dart';
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(HomeRoute.name, initialChildren: children);
+
+  static const String name = 'HomeRoute';
+  static PageInfo page = PageInfo(name, builder: (data) => const HomePage());
+}
+
+class ListingFormRoute extends PageRouteInfo<void> {
+  const ListingFormRoute({List<PageRouteInfo>? children})
+      : super(ListingFormRoute.name, initialChildren: children);
+
+  static const String name = 'ListingFormRoute';
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) => const ListingFormPage(),
+  );
+}
+
+class LoginRoute extends PageRouteInfo<void> {
+  const LoginRoute({List<PageRouteInfo>? children})
+      : super(LoginRoute.name, initialChildren: children);
+
+  static const String name = 'LoginRoute';
+  static PageInfo page = PageInfo(name, builder: (data) => const LoginPage());
+}
+
+class MainShellRoute extends PageRouteInfo<void> {
+  const MainShellRoute({List<PageRouteInfo>? children})
+      : super(MainShellRoute.name, initialChildren: children);
+
+  static const String name = 'MainShellRoute';
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) => const MainShellPage(),
+  );
+}
+
+class MarketplaceRoute extends PageRouteInfo<void> {
+  const MarketplaceRoute({List<PageRouteInfo>? children})
+      : super(MarketplaceRoute.name, initialChildren: children);
+
+  static const String name = 'MarketplaceRoute';
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) => const MarketplacePage(),
+  );
+}
+
+class ProfileRoute extends PageRouteInfo<void> {
+  const ProfileRoute({List<PageRouteInfo>? children})
+      : super(ProfileRoute.name, initialChildren: children);
+
+  static const String name = 'ProfileRoute';
+  static PageInfo page = PageInfo(name, builder: (data) => const AccountPage());
+}
+
+class RegisterRoute extends PageRouteInfo<void> {
+  const RegisterRoute({List<PageRouteInfo>? children})
+      : super(RegisterRoute.name, initialChildren: children);
+
+  static const String name = 'RegisterRoute';
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) => const RegisterPage(),
+  );
+}
+
+class SplashRoute extends PageRouteInfo<void> {
+  const SplashRoute({List<PageRouteInfo>? children})
+      : super(SplashRoute.name, initialChildren: children);
+
+  static const String name = 'SplashRoute';
+  static PageInfo page = PageInfo(name, builder: (data) => const SplashPage());
+}
