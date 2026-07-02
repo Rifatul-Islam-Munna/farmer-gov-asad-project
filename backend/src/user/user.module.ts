899d0bee75826c1b77256589e3745c8f339422b3
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-02 17:03:30 +0600

    feat(backend): wire MongoDB user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { USER_MODEL, UserSchema } from './user.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: USER_MODEL, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UserModule {}
