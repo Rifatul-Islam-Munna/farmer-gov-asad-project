bb03297dfd8192a1df4873c07ec94cce9a562d28
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-02 18:09:32 +0600

    feat(backend): add reusable role guard

---
+++ backend/src/auth/roles.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthenticatedRequest } from './access-token.guard';
+import { ROLES_KEY } from './roles.decorator';
+import { UserType } from '../user/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const allowedRoles = this.reflector.getAllAndOverride<UserType[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!allowedRoles?.length) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<AuthenticatedRequest>();
+    if (!request.user || !allowedRoles.includes(request.user.role)) {
+      throw new ForbiddenException('You do not have permission for this action');
+    }
+
+    return true;
+  }
+}
