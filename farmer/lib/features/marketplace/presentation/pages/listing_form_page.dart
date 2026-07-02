347232e5074bd73a4a2b7ce6bff5a2f464982449
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-02 19:09:03 +0600

    feat(flutter): connect role workspaces to nested third tab

--- farmer/lib/features/marketplace/presentation/pages/listing_form_page.dart
+++ farmer/lib/features/marketplace/presentation/pages/listing_form_page.dart
@@ -1,5 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../../core/storage/session_storage.dart';
+import '../../../admin/presentation/admin_workspace.dart';
+import '../../../agent/presentation/agent_assist_panel.dart';
+import '../../../medicine_seller/presentation/seller_workspace.dart';
+import '../widgets/buyer_deals_panel.dart';
+import '../widgets/sell_goods_panel.dart';
 
 @RoutePage()
 class ListingFormPage extends StatelessWidget {
@@ -7,11 +15,22 @@ class ListingFormPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Create post')),
-      body: Center(
-        child: Text('Goods posting form will be added in the next feature.'),
-      ),
-    );
+    final role = GetIt.I<SessionStorage>().role;
+    final title = switch (role) {
+      'buyer' => 'Offers and deals',
+      'agent' => 'Farmer assistance',
+      'medicineSeller' => 'Shop inventory',
+      'admin' => 'Administration',
+      _ => 'Sell goods',
+    };
+    final body = switch (role) {
+      'buyer' => const BuyerDealsPanel(),
+      'agent' => const AgentAssistPanel(),
+      'medicineSeller' => const SellerWorkspace(),
+      'admin' => const AdminWorkspace(),
+      _ => const SellGoodsPanel(),
+    };
+
+    return Scaffold(appBar: AppBar(title: Text(title)), body: body);
   }
 }
